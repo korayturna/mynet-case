59ca4f5ab5dfe280e000053304507698f3bbf92f
korayturna <korayturna@msn.com>
2022-09-13 13:28:05 +0300

    migrations created

---
+++ database/migrations/2022_09_13_094153_create_address_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->integer('person_id');
+            $table->string('address', 200);
+            $table->mediumInteger('post_code');
+            $table->unsignedTinyInteger('city_id');
+            $table->unsignedTinyInteger('country_id');
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+};
