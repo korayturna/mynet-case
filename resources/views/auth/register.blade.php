02a6b39a95f2ac73bdac5a0a2202555ec966e361
korayturna <korayturna@msn.com>
2022-09-18 17:53:53 +0300

    transla

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,7 +47,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('custom.already_registered?') }}
+                    {{ __('custom.already_registered') }}
                 </a>
 
                 <x-primary-button class="ml-4">
