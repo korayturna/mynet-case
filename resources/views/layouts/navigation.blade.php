a077ee642b7398428c837b2e01c6ac81773b0de7
korayturna <korayturna@msn.com>
2022-09-18 18:10:43 +0300

    translations

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -92,7 +92,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('custom.logout') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
